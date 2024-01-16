366465f9d40d7d821befd25c8e1c7917a86c1ba0
TuyenD <doantuyen15@gmail.com>
2024-01-16 23:58:39 +0700

    init source template

---
+++ public/utils/createMainWindow.js
@@ -0,0 +1,42 @@
+const { BrowserWindow } = require("electron");
+const { join } = require("path");
+const { autoUpdater } = require("electron-updater");
+const remote = require("@electron/remote/main");
+const config = require("./config");
+
+exports.createMainWindow = async () => {
+	const window = new BrowserWindow({
+		fullscreen: true,
+		webPreferences: {
+			nodeIntegration: true,
+			enableRemoteModule: true,
+			devTools: config.isDev,
+			contextIsolation: false,
+		},
+		frame: false,
+		icon: config.icon,
+		title: config.appName,
+	});
+
+	remote.enable(window.webContents);
+
+	await window.loadURL(
+		config.isDev
+			? "http://localhost:3000"
+			: `file://${join(__dirname, "..", "../build/index.html")}`,
+	);
+
+	window.once("ready-to-show", () => {
+		autoUpdater.checkForUpdatesAndNotify();
+	});
+
+	window.on("close", (e) => {
+		if (!config.isQuiting) {
+			e.preventDefault();
+
+			window.hide();
+		}
+	});
+
+	return window;
+};
